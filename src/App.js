d5c32eeb94a21e4e02641dd6708e9383d0af8c18
Amit Kumar Shaw <amit.shaw@rwth-aachen.de>
2024-03-09 15:53:07 +0100

    add login toggle

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import "./App.css";
-
+import React, { useState } from "react";
 import Navbar from "react-bootstrap/Navbar";
 import Container from "react-bootstrap/Container";
 import Button from "react-bootstrap/Button";
@@ -19,13 +19,18 @@ let accentColor = COLORS[OPTION].accent;
 let boxHeight = "180px";
 
 function App() {
+  const [isLoggedIn, setisLoggedIn] = useState(false);
+
+  const toggleLogin = () => {
+    setisLoggedIn(!isLoggedIn);
+  };
+
   return (
     <>
-      <div style={{ height: "100%",
-              fontFamily: "Arial" }}>
+      <div style={{ height: "100%", fontFamily: "Arial" }}>
         <Navbar sticky="top" style={{ backgroundColor: primaryColor }}>
           <Stack direction="horizontal" gap={3} className="w-100">
-            <div className="flex-grow-1"  style={{ marginLeft: "8px" }}>
+            <div className="flex-grow-1" style={{ marginLeft: "8px" }}>
               {" "}
               <img
                 src="/img/logo-light.svg"
@@ -42,30 +47,58 @@ function App() {
               }}
             >
               {" "}
-              <Button
-                variant="light"
-                style={{
-                  width: "100px",
-                  backgroundColor: secondaryColor,
-                  borderColor: secondaryColor,
-                  marginRight: "8px",
-                  color: textColor
-                }}
-              >
-                Sign up
-              </Button>
-              <Button
-                variant="light"
-                style={{
-                  width: "100px",
-                  backgroundColor: accentColor,
-                  borderColor: accentColor,
-                  marginRight: "8px",
-                  color: textColor
-                }}
-              >
-                Login
-              </Button>
+              {isLoggedIn ? (
+                <>
+                  <h3
+                    style={{
+                      fontWeight: "bold",
+                      fontSize: "24px",
+                      color: "#fafafa",
+                      marginRight: "8px",
+                    }}
+                  >
+                    Hey Human!
+                  </h3>
+                  <Button
+                    onClick={toggleLogin}
+                    variant="danger"
+                    style={{
+                      width: "100px",
+                      marginRight: "8px",
+                    }}
+                  >
+                    Logout
+                  </Button>
+                </>
+              ) : (
+                <>
+                  <Button
+                    variant="light"
+                    style={{
+                      width: "100px",
+                      backgroundColor: secondaryColor,
+                      borderColor: secondaryColor,
+                      marginRight: "8px",
+                      color: textColor,
+                    }}
+                  >
+                    Sign up
+                  </Button>
+                  <Button
+                    onClick={toggleLogin}
+                    variant="light"
+                    style={{
+                      width: "100px",
+                      backgroundColor: accentColor,
+                      borderColor: accentColor,
+                      marginRight: "8px",
+                      color: textColor,
+                    }}
+                  >
+                    Login
+                  </Button>
+                </>
+              )}
             </div>
           </Stack>
         </Navbar>
@@ -82,11 +115,21 @@ function App() {
               padding: "32px",
               backgroundColor: backgroundColor,
               flex: 1,
-              color: textColor
+              color: textColor,
             }}
           >
-            <h1 style={{fontWeight:"bold", fontSize: "64px"}}>Welcome to IntelliAssist</h1>
-            <h2 style={{fontWeight:"bold", fontSize: "36px", color: lightenColor(textColor, 0.4)}}>Your 24/7 Virtual Customer Support Partner</h2>
+            <h1 style={{ fontWeight: "bold", fontSize: "64px" }}>
+              Welcome to IntelliAssist
+            </h1>
+            <h2
+              style={{
+                fontWeight: "bold",
+                fontSize: "34px",
+                color: lightenColor(textColor, 0.4),
+              }}
+            >
+              Your 24/7 Virtual Customer Support Partner
+            </h2>
 
             <Stack direction="horizontal" gap={3}>
               <div style={{ padding: "8px", width: "50%" }}>
@@ -113,7 +156,7 @@ function App() {
                     display: "flex",
                     justifyContent: "center",
                     alignItems: "center",
-                    fontWeight: "bold"
+                    fontWeight: "bold",
                   }}
                 >
                   Why Choose IntelliAssist?
@@ -175,47 +218,49 @@ function App() {
           }}
         >
           <Stack gap={3}>
-            <div style={{ textAlign: "center", height: "90px",  }}>
-              <Container fluid>
-                <Button
-                  variant="primary"
-                  style={{
-                    width: "25%",
-                    height: "100px",
-                    textSizeAdjust: "50px",
-                    backgroundColor: primaryColor,
-                    borderColor: primaryColor,
-                    fontSize: "36px",
-                  }}
-                >
-                  Chat
-                </Button>{" "}
-                <Button
-                  variant="primary"
-                  style={{
-                    width: "25%",
-                    height: "100px",
-                    backgroundColor: primaryColor,
-                    borderColor: primaryColor,
-                    fontSize: "36px",
-                  }}
-                >
-                  Voice
-                </Button>{" "}
-                <Button
-                  variant="primary"
-                  style={{
-                    width: "25%",
-                    height: "100px",
-                    backgroundColor: primaryColor,
-                    borderColor: primaryColor,
-                    fontSize: "36px",
-                  }}
-                >
-                  Video
-                </Button>{" "}
-              </Container>
-            </div>
+            {isLoggedIn && (
+              <div style={{ textAlign: "center", height: "90px" }}>
+                <Container fluid>
+                  <Button
+                    variant="primary"
+                    style={{
+                      width: "25%",
+                      height: "100px",
+                      textSizeAdjust: "50px",
+                      backgroundColor: primaryColor,
+                      borderColor: primaryColor,
+                      fontSize: "36px",
+                    }}
+                  >
+                    Chat
+                  </Button>{" "}
+                  <Button
+                    variant="primary"
+                    style={{
+                      width: "25%",
+                      height: "100px",
+                      backgroundColor: primaryColor,
+                      borderColor: primaryColor,
+                      fontSize: "36px",
+                    }}
+                  >
+                    Voice
+                  </Button>{" "}
+                  <Button
+                    variant="primary"
+                    style={{
+                      width: "25%",
+                      height: "100px",
+                      backgroundColor: primaryColor,
+                      borderColor: primaryColor,
+                      fontSize: "36px",
+                    }}
+                  >
+                    Video
+                  </Button>{" "}
+                </Container>
+              </div>
+            )}
             <div style={{ textAlign: "center" }}>
               <h7>© IntelliAssist 2024. All rights reserved.</h7>
             </div>
